87b1f127187988de00fafd588df576c27f9a8dbd
DEV-NG <nwachukujosh2000@gmail.com>
2024-09-21 15:08:56 +0100

    main

---
+++ src/components/header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const Header = (props) => {
+  return (
+    <header id="header">
+      <div className="intro">
+        <div className="overlay">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-8 col-md-offset-2 intro-text">
+                <p className="intro-paragraph">
+"Keo Teach provides cutting-edge tech solutions, specializing in network administration, cybersecurity, and web development. Our team delivers innovative, secure, and scalable IT services to businesses. We also offer cloud solutions, IT consulting, software development, and managed services, ensuring comprehensive support for evolving digital needs."
+
+Does this fit what you had in mind?</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
